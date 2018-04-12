bad4220ce2d7b014fe1ade36216fecca26ecb565
Leapfrog-Software <Leapfrog1357>
2018-04-12 22:42:39 +0900

    [iOS] 組織種別の絞り込みを実装

--- iOS/AppFactory/AppFactory/Extension/UIColor+Original.swift
+++ iOS/AppFactory/AppFactory/Extension/UIColor+Original.swift
@@ -17,4 +17,12 @@ extension UIColor {
     class var tabUnselected: UIColor {
         return .lightGray
     }
+    
+    class var activeGreen: UIColor {
+        return UIColor(red: 31 / 255, green: 203 / 255, blue: 169 / 255, alpha: 1.0)
+    }
+    
+    class var activeGray: UIColor {
+        return UIColor(red: 191 / 255, green: 191 / 255, blue: 191 / 255, alpha: 1.0)
+    }
 }
--- iOS/AppFactory/AppFactory/View/Engineer/EngineerMenuViewController.swift
+++ iOS/AppFactory/AppFactory/View/Engineer/EngineerMenuViewController.swift
@@ -12,6 +12,15 @@ class EngineerMenuViewController: UIViewController {
 
     @IBOutlet private weak var containerLeadingConstraint: NSLayoutConstraint!
     @IBOutlet private weak var containerWidthConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var wordTextField: UITextField!
+    @IBOutlet private weak var organization1ImageView: UIImageView!
+    @IBOutlet private weak var organization2ImageView: UIImageView!
+    @IBOutlet private weak var organization3ImageView: UIImageView!
+    @IBOutlet private weak var organization1Label: UILabel!
+    @IBOutlet private weak var organization2Label: UILabel!
+    @IBOutlet private weak var organization3Label: UILabel!
+    
+    private var currentOrganization = OrganizationType.unspecified
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,6 +37,19 @@ class EngineerMenuViewController: UIViewController {
         }
     }
     
+    private func setOrganization() {
+        
+        let chackOnImage = UIImage(named: "check_on")
+        let chackOffImage = UIImage(named: "check_off")
+        
+        self.organization1ImageView.image = (self.currentOrganization == .unspecified) ? chackOnImage : chackOffImage
+        self.organization1Label.textColor = (self.currentOrganization == .unspecified) ? .activeGreen : .activeGray
+        self.organization2ImageView.image = (self.currentOrganization == .corporation) ? chackOnImage : chackOffImage
+        self.organization2Label.textColor = (self.currentOrganization == .corporation) ? .activeGreen : .activeGray
+        self.organization3ImageView.image = (self.currentOrganization == .indivisual) ? chackOnImage : chackOffImage
+        self.organization3Label.textColor = (self.currentOrganization == .indivisual) ? .activeGreen : .activeGray
+    }
+    
     private func close() {
         
         self.containerLeadingConstraint.constant = -self.containerWidthConstraint.constant
@@ -36,6 +58,21 @@ class EngineerMenuViewController: UIViewController {
         }, completion: { _ in
             self.pop(animationType: .none)
         })
+    }
+    
+    @IBAction func onTapOrganization1(_ sender: Any) {
+        self.currentOrganization = .unspecified
+        self.setOrganization()
+    }
+    
+    @IBAction func onTapOrganization2(_ sender: Any) {
+        self.currentOrganization = .corporation
+        self.setOrganization()
+    }
+    
+    @IBAction func onTapOrganization3(_ sender: Any) {
+        self.currentOrganization = .indivisual
+        self.setOrganization()
     }
     
     @IBAction func onTapSearch(_ sender: Any) {
